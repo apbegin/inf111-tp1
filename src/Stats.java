48b675f49b8a9b3db0c51baa19ff134273ab0936
apbegin <apbegin@gmail.com>
2016-10-04 00:12:12 -0400

    methode bruteforce2

--- src/Stats.java
+++ src/Stats.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Stats {
 
@@ -5,5 +6,29 @@ public class Stats {
 	public int bruteForce2LettresPlusNombre = 0;
 	public int bruteForceInterval = 0;
 	public int retroIngenierie =0;
+	public Random rnd = new Random();
 	
+	public void bruteForce(Cadenas cadenas){
+		
+		char car;
+		int position=0;
+		while(!cadenas.estOuvert()){
+			car = (char)(48 + rnd.nextInt(90));
+			
+			if(cadenas.placerCarAlaPosition(car, position)==0)
+				position+=1;
+		}
+	}
+	
+	public void bruteForceOptimise(Cadenas cadenas){
+		
+		char car;
+		int position=0;
+		
+		while(!cadenas.estOuvert()){
+			car = (char)(65+rnd.nextInt(90))
+			
+		}
+	}
 }
+
